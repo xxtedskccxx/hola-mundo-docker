21a5a8f34596188eaf8e6a9c950b0032fc91294a
xxtedskccxx <misaelbaezulloa875@gmail.com>
2026-04-23 14:29:11 -0400

    agregar prueba unitaria final

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hola Mundo DevOps </h1>');
+    res.send('<h1>Hola Mundo DevOps</h1>');
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
